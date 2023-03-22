693b4516e867eea542cd56b06079012cf712a75d
Maxim345525 <maximmaksimchyk@gmail.com>
2023-03-22 22:28:37 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 int main()
 {
-    cout << mystrlen("My name Maxim.");
+    cout << mystrlen("My name Maximm.");
     return 0;
-    cout << mystrcpy;
+    char str1[100] = "Hello";
+    char str2[100] = "Maxim";
+    cout << "Before copying->str1 = " << str1 << ", str2 = " << str2 << endl;
+    char* result = copy_string(str1, str2);
+    cout << "After copying->str1 = " << str1 << ", str2 = " << str2 << endl;
+    cout << "Result: " << result << endl;
+    char str[100] = "Hello worlds!";
+    char* result = mystrchr(str, s);
+    if (result != nullptr) {
+        cout << "S found: " << *result << endl;
+    }
+    else {
+        cout << "Character not found" << endl;
+    }
     return 0;
 }
--- string.cpp
+++ string.cpp
@@ -11,18 +11,25 @@ int mystrlen(const char* str)
 }
 char* mystrcpy(char* str1, const char* str2)
 { 
-        char* result = str1;
-        while (*str2 != '\0') {
-            *str1++ = *str2++;
-        }
-        *str1 = '\0';
-        return result;
+    strcpy(str1, str2); 
+    return str1;
 }
 char* mystrcat(char* str1, const char* str2)
 {
     while (*str2 != '\0') {
         *str1 + str2;
     }
-    return 
+    return str1;
 }
-p
+char* mystrchr(char* str, char s)
+{
+    while (*str != '\0') { 
+        if (*str == s) { 
+            return str;
+        }
+        str++; 
+    }
+    return nullptr;
+}
+
+
